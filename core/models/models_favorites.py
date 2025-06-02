73ec90d978ce89ab34a56e307f394f5c7e24dfb6
cuyssi <anabcastrolopez@gmail.com>
2025-06-02 14:51:23 +0200

    feat(CFG2-13): Create and document favorites model

---
+++ core/models/models_favorites.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+# from .models import Recipe
+from django.conf import settings
+
+class Favorite(models.Model):
+    """Modelo de Unit, representa las recetas que el usuario guarda en favoritos.  
+
+    Args:  
+        models (Model): Clase base de Django para modelos.  
+    Attributes:  
+        `user_id(ForeingKey)`: Relacion con el modelo User, que define el usuario.  
+        `created_at (DateTimeField)`: Fecha y hora de creación del registro, se establece automáticamente al crear el objeto.  
+        `updated_at (DateTimeField)`: Fecha y hora de la última actualización del registro,\n
+        se actualiza automáticamente al modificar el objeto.    
+    Author:  
+    {Ana Castro}
+    """
+
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # recipe_id = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'favorites'
+
+    """
+        Meta clase para definir metadatos del modelo Unit.
+        Esta clase define el nombre de la tabla en la base de datos.  
+        Args:  
+            db_table (str): Nombre de la tabla en la base de datos, en este caso 'units'.
+        """
