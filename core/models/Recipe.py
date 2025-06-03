0e7c8d75330ca5704dab0d554e3c92de2e38f014
Aelnordev <loreriba99@gmail.com>
2025-06-03 14:09:46 +0200

    test(CFG2-11): Test recipe model

--- core/models/Recipe.py
+++ core/models/Recipe.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
+from core.models.Category import Category
+
 
 class Recipe(models.Model):
 
@@ -25,8 +28,8 @@ class Recipe(models.Model):
     
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100, null=True, blank=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blak=True)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     duration_minutes = models.IntegerField()
     commensals = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
