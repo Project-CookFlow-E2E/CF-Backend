6e923d98d7b24c85de611fa984f48317ded7ad24
FaloCurso <falofr@gmail.com>
2025-06-04 09:40:36 +0200

    feat(CFG2-20): restructure recipe_ingredientes

---
+++ recipes/tests/test_steps.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from core.models.Step import Step
+
+class StepModelTest(TestCase):
+
+    def test_create_step(self):
+        step = Step.objects.create(
+            order=1,
+            description="Pelar las patatas"
+        )
+
+        self.assertEqual(step.order, 1)
+        self.assertEqual(step.description, "Pelar las patatas")
+        self.assertIsNotNone(step.created_at)
+        self.assertIsNotNone(step.updated_at)
