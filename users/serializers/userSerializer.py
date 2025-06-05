65c4652a2acc5412a42dce9bad5d5e721f44a617
FrostyValue <saturmendezc@gmail.com>
2025-06-05 09:49:14 +0200

    Feat (CFG2-63): Created serializer to edit users values, create new users and JWT serializer (commented password hash logic).

--- users/serializers/userSerializer.py
+++ users/serializers/userSerializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models.user import CustomUser
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     """
     	Serializer del modelo CustomUser para usuarios estandar.
@@ -33,6 +34,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = fields
 
+
 class CustomUserAdminSerializer(serializers.ModelSerializer):
     """
     	Serializer del modelo CustomUser para usuarios is_staff.
@@ -53,18 +55,116 @@ class CustomUserAdminSerializer(serializers.ModelSerializer):
             created_at – Fecha de creación del registro.
             updated_at – Fecha de última actualización del registro.
     	"""
+
     class Meta:
         model = CustomUser
         fields = '__all__'
-        read_only_fields = ['id','created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
 
 class CustomUserCreateSerializer(serializers.ModelSerializer):
+    """
+        Serializer del modelo CustomUser para la creación de usuarios estándar.
+
+        Este serializer se encarga de transformar instancias del modelo CustomUser
+        en representaciones JSON y viceversa. Se utiliza específicamente para la
+        creación de nuevos usuarios, incluyendo la gestión de la contraseña.
+
+        Campos:
+            username (str): Nombre de usuario.
+            email (str): Dirección de correo electrónico del usuario.
+            password (str): Contraseña del usuario (solo escritura).
+            name (str): Nombre de pila del usuario.
+            surname (str): Primer apellido del usuario.
+            second_surname (str): Segundo apellido del usuario.
+            biography (str): Biografía mostrada en el perfil del usuario.
+        """
     password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = CustomUser
         fields = [
             'username',
-
+            'email',
+            'name',
+            'surname',
+            'second_surname',
+            'biography',
+            'password'
         ]
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        # user.set_password(password)
+        user.save()
+        return user
+
+
+class CustomUserLoginSerializer(serializers.Serializer):
+    """
+    Serializer que permite iniciar sesión con username o email.
+        """
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user_input = data.get("username")
+        password = data.get("password")
+
+        user_obj = CustomUser.objects.filter(email=user_input).first() or CustomUser.objects.filter(
+            username=user_input).first()
+
+        if user_obj and user_obj.check_password(password):
+            if not user_obj.is_active:
+                raise serializers.ValidationError("Usuario inactivo.")
+            data["user"] = user_obj
+            return data
+
+        raise serializers.ValidationError("Credenciales inválidas.")
+
+
+class CustomUserUpdateSerializer(serializers.ModelSerializer):
+    """
+    	Serializer que permite actualizar los datos del usuario estandar.
+    	"""
+
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ['name', 'surname', 'second_surname', 'biography', 'password']
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        # if password:
+        # instance.set_password(password)
+        instance.save()
+        return instance
+
+
+class CustomUserAdminUpdateSerializer(serializers.ModelSerializer):
+    """
+        Serializer que permite actualizar los datos del usuario estandar y is_staff al usuario is_staff.
+        """
+
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'username', 'email', 'name', 'surname',
+            'second_surname', 'biography', 'is_staff', 'password'
+        ]
+        read_only_fields = ['id']
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        #if password:
+            #instance.set_password(password)
+        instance.save()
+        return instance
