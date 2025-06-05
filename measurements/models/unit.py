a80edbdf8521f3b317462c4ddb63ffe749347e8c
Algol95 <angelaragon.developer@gmail.com>
2025-06-05 09:48:56 +0200

    refactor(CFG2-75): Add user_id relathionship in unit model

--- measurements/models/unit.py
+++ measurements/models/unit.py
@@ -21,6 +21,7 @@ class Unit (models.Model):
     name = models.CharField(max_length=15, unique=True)
     unit_type = models.ForeignKey(settings.AUTH_UNITTYPE_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         
