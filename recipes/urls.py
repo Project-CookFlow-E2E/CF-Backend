e1060e8df73b795aef12ad7d9d3742f875280014
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-06-06 13:18:10 +0100

    refactor/CFG2-82/refactor code ingredientView, urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from recipes.views.ingredientViewSet import IngredientViewSet, IngredientAdminViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'ingredients', IngredientViewSet)  # Registra el ViewSet de ingredientes
 router.register(r'admin/ingredients', IngredientAdminViewSet)
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
