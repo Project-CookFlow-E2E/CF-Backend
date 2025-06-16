21916618e266c1b88faddf84dd1cca1a89966a38
FrostyValue <saturmendezc@gmail.com>
2025-06-16 17:26:48 +0200

    feature: Added endpoint to deliver random recipes and in default 5.

--- recipes/views/recipeView.py
+++ recipes/views/recipeView.py
@@ -1,6 +1,9 @@
+import random
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from recipes.models.recipe import Recipe
 from recipes.serializers.recipeSerializer import RecipeSerializer, RecipeAdminSerializer
 from media.services.image_service import update_image_for_instance
@@ -43,3 +46,28 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 external_id=recipe.id,
                 image_type="RECIPE"
             )
+
+    @action(detail=False, methods=['get'])
+    def random(self, request):
+        """
+        Returns a specified number of random recipes by picking random IDs.
+        Query parameter 'count' (default: 5) to specify how many random recipes.
+        This method is more efficient than order_by('?') for large tables.
+        Filters (like user_id or current user) applied via get_queryset will work as expected.
+        """
+        count = int(request.query_params.get('count', 5))
+
+        all_recipe_ids = list(self.get_queryset().values_list('id', flat=True))
+
+        if not all_recipe_ids:
+            return Response([])
+
+        if len(all_recipe_ids) <= count:
+            random_ids = all_recipe_ids
+        else:
+            random_ids = random.sample(all_recipe_ids, count)
+
+        random_recipes = self.get_queryset().filter(id__in=random_ids)
+
+        serializer = self.get_serializer(random_recipes, many=True)
+        return Response(serializer.data)
