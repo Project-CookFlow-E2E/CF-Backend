414af0d369a35d5874575c312474cb981f23886c
hema <hemapriyaweb@gmail.com>
2025-06-28 19:52:48 +0200

    test: local backend connected

--- config/settings.py
+++ config/settings.py
@@ -44,8 +44,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
-
+    "http://127.0.0.1:5173",
+    "http://localhost",
+    "http://127.0.0.1", 
 ]
 
 ROOT_URLCONF = 'config.urls'
