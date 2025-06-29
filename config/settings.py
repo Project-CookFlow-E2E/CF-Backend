d2e4014c56caae6b3136ffea3e0818a8b96c60af
hema <hemapriyaweb@gmail.com>
2025-06-29 02:41:01 +0200

    test: updated local & docker tests - working

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost",
+    "http://localhost:80",
     "http://127.0.0.1", 
+    "http://frontend",
 ]
 
 ROOT_URLCONF = 'config.urls'
